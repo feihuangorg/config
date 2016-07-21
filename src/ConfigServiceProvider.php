ce755716c942ae08825cbbbdb16ea0b14de23ae6
Larry <lifushui@qq.com>
2016-07-21 22:12:58 +0800

    no message

--- src/ConfigServiceProvider.php
+++ src/ConfigServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace October\Rain\Config;
+namespace Feihuangorg\Config;
 
 use Illuminate\Support\ServiceProvider;
 
--- src/FileWriter.php
+++ src/FileWriter.php
@@ -1,4 +1,4 @@
-<?php namespace October\Rain\Config;
+<?php namespace Feihuangorg\Config;
 
 use Illuminate\Filesystem\Filesystem;
 
